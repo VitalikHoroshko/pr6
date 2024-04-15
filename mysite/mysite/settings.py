948d3ae82f66f2abc63c6cda61b8ed7193912ec7
Boychuk Vitalii <119891635+VitalikHoroshko@users.noreply.github.com>
2024-04-15 17:32:05 +0300

    Add files via upload

---
+++ mysite/mysite/settings.py
@@ -0,0 +1,10 @@
+# mysite/settings.py
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app_blog'
+]
